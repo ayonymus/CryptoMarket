3d6a393628d43481473223a60af0ddfb66631f6b
ayonymus <ayonymus@gmail.com>
2022-11-14 09:47:00 +0100

    Create App class

---
+++ app/src/main/java/com/ayon/cryptomarket/App.kt
@@ -0,0 +1,24 @@
+package com.ayon.cryptomarket
+
+import android.app.Application
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class App : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidContext(this@App)
+            modules(
+            )
+        }
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
